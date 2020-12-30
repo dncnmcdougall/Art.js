c2d7f9b68476262fc843db0cd2f16bcd593f9eb9
Duncan McDougall <dncn.mcdougall@gmail.com>
2020-12-30 14:53:48 +0000

    Separation, Cohesion and Alignment.
    The decisions were broken up into the three parts that are weighted
    separately: Separation, Cohesion and Alignment.
    
    Much duplicate code was moved into functions.

--- src/bird.js
+++ src/bird.js
@@ -9,43 +9,83 @@ function distanceCostDir( d, size) {
     return (-(x)*(x-10))*Math.exp(-x/4)/10;
 }
 
-const Bird = function(pos, vec, size, colour, name) {
+function sigma(d, size, L /*Max value*/, x0 /*centre*/, k /*steepness*/) {
+    x = d/size;
+    return L/(1+Math.exp(-k*(x-x0)));
+}
+
+const Bird = function(pos, vec, size, colour, name, 
+    seperation_settings, cohesion_settings, alignment_settings ) {
     return {
         'Pos': pos,
         'Vec': vec,
         'Size': size,
         'Colour': colour,
+        'Seperation': seperation_settings,
+        'Cohesion': cohesion_settings,
+        'Alignment': alignment_settings,
         'Name': name,
         'draw': function( ctx ) {
-            ctx.strokeStyle = colour;
+            ctx.strokeStyle = this.Colour;
             // ctx.fillStyle = colour;
 
-            var normal = Vector.mult(vec, size);
+            var normal = Vector.mult(this.Vec, this.Size);
             ctx.beginPath();
-            ctx.moveTo(pos.x-normal.y/3,pos.y+normal.x/3);
-            ctx.lineTo(pos.x+normal.x/2,pos.y+normal.y/2);
-            ctx.lineTo(pos.x+normal.y/3,pos.y-normal.x/3);
+            ctx.moveTo(this.Pos.x-normal.y/3,this.Pos.y+normal.x/3);
+            ctx.lineTo(this.Pos.x+normal.x/2,this.Pos.y+normal.y/2);
+            ctx.lineTo(this.Pos.x+normal.y/3,this.Pos.y-normal.x/3);
             ctx.stroke();
-            if (name ){
-                ctx.strokeText(name, pos.x, pos.y);
+
+            if ( this.Name ){
+                ctx.strokeText(this.Name, this.Pos.x, this.Pos.y);
             }
         },
         'move': function(time, width, height) {
-            pos = Vector.add(pos,Vector.mult(vec,time))
-            pos = Vector.wrap(pos, width, height);
+            this.Pos = Vector.add(this.Pos,Vector.mult(this.Vec,time))
+            this.Pos = Vector.wrap(this.Pos, width, height);
         },
-        'update': function(bird, bird_count, print) {
-            var distance = Vector.distance(pos,bird.Pos);
-            var dist_dir = Vector.norm(Vector.sub(bird.Pos, pos));
-            let dir_cost = distanceCostDir(distance, size);
-            let dist_cost = distanceCost(distance, size);
-            vec = Vector.weightedAdd(vec,1, bird.Vec, dir_cost/bird_count);
+        'distance': function(otherPos) {
+            return Vector.distance(this.Pos, otherPos);
+        },
+        'sigmas': function(otherBird) {
+            let d = this.distance(otherBird.Pos);
 
-            vec = Vector.weightedAdd(vec,1, dist_dir, dist_cost/bird_count);
-            vec = Vector.norm(vec);
-            if ( print ) {
-                console.log(distance/size, dist_cost, dir_cost);
-            }
+            seperation_sigma = sigma(d, this.Size, 1, this.Seperation.r, this.Seperation.k);
+            cohesion_sigma = sigma(d, this.Size, 1, this.Cohesion.r, this.Cohesion.k);
+            alignment_sigma = sigma(d, this.Size, 1, this.Alignment.r, this.Alignment.k);
+
+            return [seperation_sigma, cohesion_sigma, alignment_sigma];
+        },
+        'accumulate': function(seperation_total, seperation_centre,
+            cohesion_total, cohesion_centre,
+            alignment_total, alignment_centre, alignment_vector) {
+
+            seperation_centre = Vector.mult(seperation_centre, 1/seperation_total);
+            cohesion_centre = Vector.mult(cohesion_centre, 1/cohesion_total);
+            alignment_centre = Vector.mult(alignment_centre, 1/alignment_total);
+
+            let seperation_vector = Vector.norm(Vector.sub(this.Pos, seperation_centre));
+            let seperation_distance = this.distance(seperation_centre);
+            let seperation_sigma = sigma(seperation_distance, this.Size, 1, this.Seperation.r, this.Seperation.k);
+
+            let cohesion_vector = Vector.norm(Vector.sub(cohesion_centre, this.Pos));
+            let cohesion_distance = this.distance(cohesion_centre);
+            let cohesion_sigma = sigma(cohesion_distance, this.Size, 1, this.Cohesion.r, this.Cohesion.k);
+
+            alignment_vector = Vector.norm(alignment_vector);
+            let alignment_distance = this.distance(alignment_centre);
+            let alignment_sigma = sigma(alignment_distance, this.Size, 1, this.Alignment.r, this.Alignment.k);
+
+
+            this.Vec = Vector.norm(
+                Vector.multAdd(this.Vec, 1, 
+                    Vector.multAdd(seperation_vector, this.Seperation.w*seperation_sigma, 
+                        Vector.multAdd(cohesion_vector, this.Cohesion.w*cohesion_sigma, 
+                            alignment_vector, this.Alignment.w*alignment_sigma),
+                        1),
+                    1)
+            );
+
         }
     };
 
--- src/flock.js
+++ src/flock.js
@@ -1,14 +1,35 @@
-var maxPos = Vector.toVec( 650, 650);
+var maxPos = Vector.toVec( 500, 500);
 
 var canvas = document.getElementById('canvas');
 canvas.width = maxPos.x;
 canvas.height = maxPos.y;
 var ctx = canvas.getContext('2d');
 
-var interval = 50;
-var speed = 2;
+const v2str = Vector.toString;
 
-var count = 100;
+const seperation_settings = {
+    'r': 1,
+    'k': -2,
+    'w': 0.7
+};
+const cohesion_settings = {
+    'r': 5,
+    'k': -1,
+    'w': 0.3
+};
+const alignment_settings = {
+    'r': 5,
+    'k': -1,
+    'w': 0.1
+};
+
+var interval = 50;
+
+const speed = 2;
+const count = 100;
+const size = 10;
+const name = false;
+
 var birds = []
 var colours = [
 '#FF007F',
@@ -28,11 +49,28 @@ for( let i = 0; i < count; i++) {
     vec = Vector.randomDir(1)
     var colour = i%colours.length;
 
-    birds.push( Bird(pos, vec, 20, colours[colour]))
+    birds.push( Bird(pos, vec, size, colours[colour], name?''+i:null,
+    seperation_settings, cohesion_settings, alignment_settings))
+}
+
+function addBird(distance, size, vector, total, total_vector, x0, k, print) {
+    let sig = sigma(distance, size, 1, x0, k);
+    if ( print) {
+        console.log('dist', distance, 'size', size);
+        console.log('d', distance/size, 'sig', sig);
+        console.log('t', total, 'tv', total_vector, 'v', vector);
+    }
+    total += sig;
+    total_vector = Vector.multAdd(total_vector, 1, vector, sig);
+    if ( print) {
+        console.log('t', total, 'tv', total_vector, 'v', vector);
+    }
+    return [total, total_vector];
+
 }
 
 var ite = 0;
-var timer = setInterval( () => {
+function updateFunction() {
     // Clear canvas
     ctx.fillStyle = '#FFFFFF';
     ctx.fillRect(0,0, maxPos.x, maxPos.y);
@@ -43,15 +81,40 @@ var timer = setInterval( () => {
         birds[i].move(speed, maxPos.x, maxPos.y);
     }
     for( i in birds) {
-    for( j in birds) {
-        if( i == j ) continue;
-        birds[i].update(birds[j], count);
-    }
+        let bird = birds[i];
+        let seperation_centre = Vector.toVec(0,0);
+        let seperation_total = 0;
+        let cohesion_centre = Vector.toVec(0,0);
+        let cohesion_total = 0;
+        let alignment_centre = Vector.toVec(0,0);
+        let alignment_vector = Vector.toVec(0,0);
+        let alignment_total = 0;
+
+        for( j in birds) {
+            if( i == j ) continue;
+    
+            let sigmas = bird.sigmas(birds[j]);
+            seperation_total += sigmas[0];
+            seperation_centre = Vector.multAdd(seperation_centre,1, birds[j].Pos, sigmas[0]);
+
+            cohesion_total += sigmas[1];
+            cohesion_centre = Vector.multAdd(cohesion_centre,1, birds[j].Pos, sigmas[1]);
+
+            alignment_total += sigmas[2];
+            alignment_centre = Vector.multAdd(alignment_centre,1, birds[j].Pos, sigmas[2]);
+            alignment_vector = Vector.multAdd(alignment_vector,1, birds[j].Vec, sigmas[2]);
+
+        }
+        birds[i].accumulate(seperation_total, seperation_centre,
+            cohesion_total, cohesion_centre,
+            alignment_total, alignment_centre, alignment_vector);
     }
 
     ite+=1;
     // if(ite > 100) {
     //     clearInterval(timer);
     // }
+}
+// updateFunction();
 
-}, interval);
+var timer = setInterval( updateFunction, interval);
